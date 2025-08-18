b8082731863505a414b07d251f503b6d9128d1ba
liy <18536431616@163.com>
2025-08-18 18:04:09 +0800

    fsadsa

---
+++ out/_next/static/AZ_3l9t4ulsGXEY8yYdpl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/6b8ac048ebb19399.css","static/chunks/pages/index-39aa1723e8b24d20.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
